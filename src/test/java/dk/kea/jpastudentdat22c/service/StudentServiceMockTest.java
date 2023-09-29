c7140b4a3a8600869cc9cd2d5804c962065e7c8b
jarltuxen <jarl.tuxen@gmail.com>
2023-09-29 15:31:40 +0200

    demo Mock af deleteById

--- src/test/java/dk/kea/jpastudentdat22c/service/StudentServiceMockTest.java
+++ src/test/java/dk/kea/jpastudentdat22c/service/StudentServiceMockTest.java
@@ -23,6 +23,8 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doThrow;
+
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
@@ -58,12 +60,17 @@ class StudentServiceMockTest {
         List<Student> studentList = new ArrayList<>();
         studentList.add(s1);
         studentList.add(s2);
-
+        //findall giver liste af studerende
         Mockito.when(mockedStudentRepository.findAll()).thenReturn(studentList);
+        //findById giver studerende på id=1 og empty på id=42
         Mockito.when(mockedStudentRepository.findById(1)).thenReturn(Optional.of(s1));
         Mockito.when(mockedStudentRepository.findById(42)).thenReturn(Optional.empty());
+        //deleteById giver empty hvis id = 42 - doThrow bruges, da deleteById er void
+        //Mockito.when(mockedStudentRepository.deleteById(42)).thenThrow(new StudentNotFoundException("Student not found with id: 42"));
+        doThrow(new StudentNotFoundException("Student not found with id: 42")).when(mockedStudentRepository).deleteById(42);
 
         // Define the behavior using thenAnswer
+        // The student passed in save, can be read from arguments in the InvocationOnMock object
         Mockito.when(mockedStudentRepository.save(ArgumentMatchers.any(Student.class))).thenAnswer(new Answer<Student>() {
             @Override
             public Student answer(InvocationOnMock invocation) throws Throwable {
@@ -71,8 +78,11 @@ class StudentServiceMockTest {
                 Object[] arguments = invocation.getArguments();
                 if (arguments.length > 0 && arguments[0] instanceof Student) {
                     Student studentToSave = (Student) arguments[0];
-                    //repository skal returnere studentobject med næste ledige id = 3
-                    studentToSave.setId(3);
+                    //er id = 0, så simuler create - er id sat så simuler opdater
+                    if (studentToSave.getId()==0) {
+                        //create - repository skal returnere studentobject med næste ledige id = 3
+                        studentToSave.setId(3);
+                    }
                     return studentToSave;
                 } else {
                     // Handle the case where the argument is not a Student (optional)
@@ -80,9 +90,7 @@ class StudentServiceMockTest {
                 }
             }
         });
-
-// Now you can use your mocked StudentRepository for testing
-
+        //inject mocked repository and other dependencies in studentService
         studentService = new StudentService(mockedStudentRepository, studentConverter);
 
     }
@@ -122,11 +130,22 @@ class StudentServiceMockTest {
     void updateStudent() {
         //kald studentService.save og assertEquals på studentDTO - husk init Mockito.when.thenReturn
         //lav test for eksisterende og ikke eksisterende student (assertThrows)
+        StudentDTO resultStudentDTO = studentService.updateStudent(1, studentConverter.toDTO(
+                new Student(
+                        1,
+                        "Hugo",
+                        LocalDate.of(2000,1,1),
+                        LocalTime.of(0, 0, 1)
+                )));
+        assertEquals(1, resultStudentDTO.id());
+        assertEquals("Hugo", resultStudentDTO.name());
+        assertThrows(StudentNotFoundException.class, () -> studentService.updateStudent(42, resultStudentDTO));
     }
 
     @Test
     void deleteStudentById() {
         //kald studentService.delete
         //test at der kommer StudentNotFoundException for ikke-eksisterende student
+        assertThrows(StudentNotFoundException.class, () -> studentService.deleteStudentById(42));
     }
 }
