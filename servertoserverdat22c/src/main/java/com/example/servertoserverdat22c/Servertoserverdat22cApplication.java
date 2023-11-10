52f8b3b66b74db45fbaa25b043813d8ac3f347a5
jarltuxen <jarl.tuxen@gmail.com>
2023-11-10 10:45:58 +0100

    tutorial part 1

---
+++ servertoserverdat22c/src/main/java/com/example/servertoserverdat22c/Servertoserverdat22cApplication.java
@@ -0,0 +1,13 @@
+package com.example.servertoserverdat22c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Servertoserverdat22cApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Servertoserverdat22cApplication.class, args);
+    }
+
+}
---
+++ servertoserverdat22c/src/test/java/com/example/servertoserverdat22c/Servertoserverdat22cApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servertoserverdat22c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Servertoserverdat22cApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
